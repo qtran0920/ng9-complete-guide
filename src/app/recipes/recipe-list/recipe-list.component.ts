bcd526b959b27178254435b238ab08eebf7e85bc
Jimmy Tran <qtran11@gmu.edu>
2020-04-22 20:31:44 -0400

    feat(recipe-list.component): output a list of recipes using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,8 +7,9 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
-
+  recipes: Recipe[] = [
+    new Recipe('Irish Creme Chocolate Trifle', 'I was given a bottle of Irish cream liqueur as a gift and had leftover peppermint candy, so I created this delicious trifle. It\'s always rich and decadent. —Margaret Wilson, Sun City, California', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Irish-Creme-Chocolate-Trifle_EXPS_SDDJ18_28545_C08_03_9b-2-696x696.jpg'),
+    new Recipe('Irish Creme Chocolate Trifle', 'I was given a bottle of Irish cream liqueur as a gift and had leftover peppermint candy, so I created this delicious trifle. It\'s always rich and decadent. —Margaret Wilson, Sun City, California', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Irish-Creme-Chocolate-Trifle_EXPS_SDDJ18_28545_C08_03_9b-2-696x696.jpg')];
   constructor() { }
 
   ngOnInit(): void {
